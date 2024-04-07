32706258b6d0f599416fe0d63e59b0521f9fa579
Shivam562000 <shivamchougule.200056@gmail.com>
2024-04-07 22:22:42 +0530

    Moved files in separate folder

--- Creational Patterns/SingletonPattern/SingletonPattern.java
+++ Creational Patterns/SingletonPattern/SingletonPattern.java
@@ -1,19 +1,26 @@
+package SingletonPattern;
+
 // Double Checked Locking based Java implementation of
 // singleton design pattern
 import java.io.*;
+
 class Singleton {
+    /*
+     * We have declared the obj volatile which ensures that multiple threads offer
+     * the obj variable correctly when it is being initialized to the Singleton
+     * instance. This method drastically reduces the overhead of calling the
+     * synchronized method every time.
+     */
     private static volatile Singleton obj = null;
-    private Singleton() 
-    {
+
+    private Singleton() {
         System.out.println("Singleton is Instantiated.");
     }
 
-    public static Singleton getInstance()
-    {
+    public static Singleton getInstance() {
         if (obj == null) {
             // To make thread safe
-            synchronized (Singleton.class)
-            {
+            synchronized (Singleton.class) {
                 // check again as multiple threads
                 // can reach above step
                 if (obj == null)
@@ -23,15 +30,13 @@ class Singleton {
         return obj;
     }
 
-    public static void doSomething()
-    {
+    public static void doSomething() {
         System.out.println("Somethong is Done.");
     }
 }
 
 class SingletonPattern {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Singleton.getInstance().doSomething();
     }
 }
